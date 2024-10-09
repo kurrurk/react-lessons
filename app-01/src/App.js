ec596dcf5a5487e166a898e92f2062e4f993bf38
kurrurk <kuremkarmerurk@gmail.com>
2024-10-09 12:39:11 +0200

    App 1 ( lessons 17 - 20 : CSS, JSX )

--- app-01/src/App.js
+++ app-01/src/App.js
@@ -1,7 +1,12 @@
+import CostItem from "./components/CostItem";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello world!!!</h1>
+      <CostItem></CostItem>
+      <CostItem></CostItem>
+      <CostItem></CostItem>
     </div>
   );
 }
---
+++ app-01/src/components/CostItem.js
@@ -0,0 +1,20 @@
+import "./CostItem.css";
+
+function CostItem () {
+    const costDate = new Date(2024, 2, 15);
+    const costDescription = "Kühlschrank";
+    const costAmmount = 999.99;
+
+    return (
+        <div className="cost-item" >
+            <div>{costDate.toDateString()}</div>
+            <div className="cost-item__description">
+                <h2>{costDescription}</h2>
+                <div className="cost-item__price">{costAmmount}€</div>
+            </div>
+        </div>
+    );
+
+}
+
+export default CostItem;
