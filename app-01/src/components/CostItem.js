2ca49898ea36b1dda5e0ee4ad334beed446dbef2
kurrurk <kuremkarmerurk@gmail.com>
2024-10-09 18:12:55 +0200

    App 1 ( lesson 22 : get month,year,day )

--- app-01/src/components/CostItem.js
+++ app-01/src/components/CostItem.js
@@ -2,9 +2,17 @@ import "./CostItem.css";
 
 function CostItem (props) {
 
+    const month = props.date.toLocaleString('de-De', {month: 'long'} );
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString('de-De', {day: '2-digit'} );
+
     return (
         <div className="cost-item" >
-            <div>{props.date.toDateString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className="cost-item__description">
                 <h2>{props.description}</h2>
                 <div className="cost-item__price">{props.ammount}€</div>
