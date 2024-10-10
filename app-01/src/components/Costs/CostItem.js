83e1eaf0e56c143b7790b319964a2f5553e42408
kurrurk <kuremkarmerurk@gmail.com>
2024-10-10 20:51:44 +0200

    App 1 (lesson 32 : события в реакте)

--- app-01/src/components/Costs/CostItem.js
+++ app-01/src/components/Costs/CostItem.js
@@ -4,13 +4,22 @@ import Card from "../UI/Card";
 
 const CostItem = (props) => {    
 
+    const changeDescriptionHandler = () => {
+        console.log('Click texs!!!')
+    }
+
     return (
         <Card className="cost-item">
             <CostDate date={props.date} />
             <div className="cost-item__description">
                 <h2>{props.description}</h2>
-                <div className="cost-item__price">{props.ammount}€</div>
+                <div className="cost-item__price">{props.ammount}€</div>                                
             </div>
+            <button 
+                onClick={changeDescriptionHandler}
+            >
+                Beschreibung ändern
+            </button>
         </Card>
     );
 
