153adf77fb91fec7d5a7550df36a289e0cdd4f9f
kurrurk <kuremkarmerurk@gmail.com>
2024-10-10 21:14:34 +0200

    App 1 (lesson 33 - 35 : hook "useState")

--- app-01/src/components/Costs/CostItem.js
+++ app-01/src/components/Costs/CostItem.js
@@ -1,18 +1,23 @@
 import "./CostItem.css";
 import CostDate from "./CostDate";
 import Card from "../UI/Card";
+import React, {useState} from "react";
 
 const CostItem = (props) => {    
 
+    const [description, setDescription] = useState(props.description);
+
     const changeDescriptionHandler = () => {
-        console.log('Click texs!!!')
+        // description = 'Test text!!!';
+        setDescription('Test text!!!'); // description = 'Test text!!!'; & CostIten(props);
+        console.log(description); // отображает старое значение
     }
 
     return (
         <Card className="cost-item">
             <CostDate date={props.date} />
             <div className="cost-item__description">
-                <h2>{props.description}</h2>
+                <h2>{description}</h2>
                 <div className="cost-item__price">{props.ammount}€</div>                                
             </div>
             <button 
