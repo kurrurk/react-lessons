eefc46000e4e27b3552fa5a321c2a98ae7803ae1
kurrurk <kuremkarmerurk@gmail.com>
2024-10-13 23:43:49 +0200

    App1 (Lessons 40-42 : работа с состояниями и двустороннее связывание)

--- app-01/src/components/NewCost/CostForm.js
+++ app-01/src/components/NewCost/CostForm.js
@@ -3,66 +3,110 @@ import './CostForm.css';
 
 const CostForm = () => {
 
-    //const [name, setName] = useState('');
-    //const [amount, setAmount] = useState('');
-    //const [date, setDate] = useState('');
+    const [inputName, setInputName] = useState('');
+    const [inputAmount, setInputAmount] = useState('');
+    const [inputDate, setInputDate] = useState('');
 
-    const [userInput, setUserInput] = useState({
-        name: '',
-        amount: '',
-        date: ''
-    });
+    //const [userInput, setUserInput] = useState({
+    //    name: '',
+    //    amount: '',
+    //    date: ''
+    //});
 
     const nameChangeHandler = (event) => {
-        //setName(event.target.value);
-        setUserInput({
-            ...userInput,
-            name: event.target.value
-        });
+        setInputName(event.target.value);
+        // не корректный способ, из-за таймингов обновления состояния
+        //setUserInput({
+        //    ...userInput,
+        //    name: event.target.value
+        //});
+        // корректный способ : 
+        //setUserInput((previousState) => {
+        //    return {
+        //        ...previousState,
+        //        name: event.target.value
+        //    }
+        //});
     }    
 
     const amountChangeHandler = (event) => {
-        //setAmount(event.target.value);
-        setUserInput({
-            ...userInput,
-            amount: event.target.value
-        });
+        setInputAmount(event.target.value);
+        // не корректный способ, из-за таймингов обновления состояния
+        //setUserInput({
+        //    ...userInput,
+        //    amount: event.target.value
+        //});
+        // корректный способ : 
+        //setUserInput((previousState) => {
+        //    return {
+        //        ...previousState,
+        //        amount: event.target.value
+        //    }
+        //});
     }    
 
     const dateChangeHandler = (event) => {
-        //setDate(event.target.value);
-        setUserInput({
-            ...userInput,
-            date: event.target.value
-        });
+        setInputDate(event.target.value);
+        // не корректный способ, из-за таймингов обновления состояния
+        //setUserInput({
+        //    ...userInput,
+        //    date: event.target.value
+        //});
+        // корректный способ : 
+        //setUserInput((previousState) => {
+        //    return {
+        //        ...previousState,
+        //        date: event.target.value
+        //    }
+        //});
+
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const costData = {
+            name: inputName,
+            amount: inputAmount,
+            date: new Date(inputDate) 
+        }
+
+        console.log( '--------------------' );
+        console.log( costData );
+        console.log( '--------------------' );
+
+        setInputName('');
+        setInputAmount('');
+        setInputDate('');
+
     }
 
     console.log( '--------------------' );
-    //console.log( 'name => ' + name );
-    console.log( 'name => ' + userInput.name );
-    //console.log( 'amount => ' + amount );
-    console.log( 'name => ' + userInput.amount);
-    //console.log( 'date => ' + date );
-    console.log( 'name => ' + userInput.date );
+    console.log( 'name => ' + inputName );
+    //console.log( 'name => ' + userInput.name );
+    console.log( 'amount => ' + inputAmount );
+    //console.log( 'name => ' + userInput.amount);
+    console.log( 'date => ' + inputDate );
+    //console.log( 'name => ' + userInput.date );
     console.log( '--------------------' );
 
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-cost__controls'>
                 
             <div className='new-cost__control'>
                  <label htmlFor="">Artikel</label>
-                 <input type="text" onChange={nameChangeHandler}/>
+                 <input type="text" value={inputName} onChange={nameChangeHandler}/>
             </div>  
 
             <div className='new-cost__control'>
                  <label htmlFor="">Summe</label>
-                 <input type="number" min='0.01' step='0.01' onChange={amountChangeHandler} />
+                 <input type="number" min='0.01' step='0.01' value={inputAmount} onChange={amountChangeHandler} />
             </div>  
 
             <div className='new-cost__control'>
                  <label htmlFor="">Datum</label>
-                 <input type="date" min="2019-01-01" step="2024-12-31" onChange={dateChangeHandler} />
+                 <input type="date" min="2019-01-01" step="2024-12-31" value={inputDate} onChange={dateChangeHandler} />
             </div>  
 
             <div className='new-cost__actions'>
