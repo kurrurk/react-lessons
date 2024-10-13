5a74d2af79cfbfa824036a10ff45d2737d70a12c
kurrurk <kuremkarmerurk@gmail.com>
2024-10-13 12:32:43 +0200

    App1 (lesson 37 - 39 : прослушивание ввода ползователя, состояния)

--- app-01/src/components/NewCost/CostForm.js
+++ app-01/src/components/NewCost/CostForm.js
@@ -1,23 +1,68 @@
+import { useState } from 'react';
 import './CostForm.css';
 
 const CostForm = () => {
+
+    //const [name, setName] = useState('');
+    //const [amount, setAmount] = useState('');
+    //const [date, setDate] = useState('');
+
+    const [userInput, setUserInput] = useState({
+        name: '',
+        amount: '',
+        date: ''
+    });
+
+    const nameChangeHandler = (event) => {
+        //setName(event.target.value);
+        setUserInput({
+            ...userInput,
+            name: event.target.value
+        });
+    }    
+
+    const amountChangeHandler = (event) => {
+        //setAmount(event.target.value);
+        setUserInput({
+            ...userInput,
+            amount: event.target.value
+        });
+    }    
+
+    const dateChangeHandler = (event) => {
+        //setDate(event.target.value);
+        setUserInput({
+            ...userInput,
+            date: event.target.value
+        });
+    }
+
+    console.log( '--------------------' );
+    //console.log( 'name => ' + name );
+    console.log( 'name => ' + userInput.name );
+    //console.log( 'amount => ' + amount );
+    console.log( 'name => ' + userInput.amount);
+    //console.log( 'date => ' + date );
+    console.log( 'name => ' + userInput.date );
+    console.log( '--------------------' );
+
     return (
         <form>
             <div className='new-cost__controls'>
                 
             <div className='new-cost__control'>
                  <label htmlFor="">Artikel</label>
-                 <input type="text" />
+                 <input type="text" onChange={nameChangeHandler}/>
             </div>  
 
             <div className='new-cost__control'>
                  <label htmlFor="">Summe</label>
-                 <input type="number" min='0.01' step='0.01' />
+                 <input type="number" min='0.01' step='0.01' onChange={amountChangeHandler} />
             </div>  
 
             <div className='new-cost__control'>
                  <label htmlFor="">Datum</label>
-                 <input type="date" min="2019-01-01" step="2024-12-31" />
+                 <input type="date" min="2019-01-01" step="2024-12-31" onChange={dateChangeHandler} />
             </div>  
 
             <div className='new-cost__actions'>
