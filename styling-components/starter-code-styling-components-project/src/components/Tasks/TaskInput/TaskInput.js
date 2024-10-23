277ad46e06bcfa042bf69739a7de5c5006f3b5ed
kurrurk <kuremkarmerurk@gmail.com>
2024-10-24 00:12:41 +0200

    Styling-components (Lesson 62 : Динамическое Добавление Стилей Inline )

--- styling-components/starter-code-styling-components-project/src/components/Tasks/TaskInput/TaskInput.js
+++ styling-components/starter-code-styling-components-project/src/components/Tasks/TaskInput/TaskInput.js
@@ -4,22 +4,31 @@ import Button from "../../UI/Button/Button";
 import "./TaskInput.css";
 
 const TaskInput = (props) => {
-  const [inputText, setinputText] = useState("");
+  const [inputText, setInputText] = useState("");
+  const [isInputValid, setisInputValid] = useState(true);
 
   const taskInputChangeHandler = (event) => {
-    setinputText(event.target.value);
+    setInputText(event.target.value);
+    setisInputValid(true);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (inputText.trim().length === 0) {
+      setisInputValid(false);
+      return ;
+    }    
     props.onAddTask(inputText);
+    setInputText('');
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Задачи</label>
-        <input type="text" onChange={taskInputChangeHandler} />
+        <label style={{color: !isInputValid ? 'red' : 'black' }} >Задачи</label>
+        <input value={inputText} style={{
+          backgroundColor: !isInputValid ? '#ff000044' : 'transparent',
+          borderColor: !isInputValid ? 'red' : '' }} type="text" onChange={taskInputChangeHandler} />
       </div>
       <Button type="submit">Добавить Задачу</Button>
     </form>
