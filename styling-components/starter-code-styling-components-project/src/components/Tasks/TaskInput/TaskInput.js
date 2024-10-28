353bc9e9de9430c5ef283a643373b538d82edc08
kurrurk <kuremkarmerurk@gmail.com>
2024-10-29 00:49:03 +0100

    styling-components (Lessons: 66-69 : CSS modules und  Media Queries)

--- styling-components/starter-code-styling-components-project/src/components/Tasks/TaskInput/TaskInput.js
+++ styling-components/starter-code-styling-components-project/src/components/Tasks/TaskInput/TaskInput.js
@@ -1,36 +1,35 @@
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import "./TaskInput.css";
-import styled from "styled-components";
+import styles from "./TaskInput.module.css";
+//import styled from "styled-components";
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${props => props.invalid ? 'red' : 'black'};
+//   }
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${props => props.invalid ? 'red' : 'black'};
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+//     background-color: ${props => props.invalid ? '#ff000044' : 'transparent'};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
-    background-color: ${props => props.invalid ? '#ff000044' : 'transparent'};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-
-  & input:focus {
-    outline: none;
-    background: #c8e1e4;
-    border-color: #00358b;
-  }
-`;
+//   & input:focus {
+//     outline: none;
+//     background: #c8e1e4;
+//     border-color: #00358b;
+//   }
+// `;
 
 const TaskInput = (props) => {
   const [inputText, setInputText] = useState("");
@@ -60,10 +59,14 @@ const TaskInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       {/* <FormControl className={!isInputValid && 'invalid'}> */}
-      <FormControl invalid={!isInputValid}>
+      {/* <FormControl invalid={!isInputValid}>
         <label>Задачи</label>
         <input value={inputText} type="text" onChange={taskInputChangeHandler} />
-      </FormControl>
+      </FormControl> */}
+      <div className={`${styles["form-control"]} ${!isInputValid && styles.invalid}`}>
+        <label>Задачи</label>
+        <input value={inputText} type="text" onChange={taskInputChangeHandler} />
+      </div>
       <Button type="submit">Добавить Задачу</Button>
     </form>
   );
