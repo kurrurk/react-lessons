39dbec51cd035071dced8c5aba0c8007fff26aea
kurrurk <kuremkarmerurk@gmail.com>
2024-12-02 01:13:01 +0100

    http-requests-starter (Lessons 160-161 : Использование useEffect для Запросов и Подготовка Проекта к Следующим Шагам)

--- http-requests-starter/src/App.js
+++ http-requests-starter/src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect,useCallback } from "react";
 
 import JokeList from "./components/JokeList";
 import "./App.css";
+import AddJoke from "./components/AddJoke";
 
 function App() {
   /* const dummyJokes = [
@@ -22,6 +23,7 @@ function App() {
 
   const [jokes, setJokes] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null); 
 
   /* const fetchJokesHandler = () => {
     fetch('https://official-joke-api.appspot.com/random_ten').then(responce => {
@@ -31,24 +33,56 @@ function App() {
     });
   } */
 
-  async function fetchJokesHandler() {
+  const fetchJokesHandler = useCallback( async () => {
     setIsLoading(true);
-    const responce = await fetch('https://official-joke-api.appspot.com/random_ten');
-    const data = await responce.json();
-    setJokes(data);
+    setError(null)
+    try {    
+      const responce = await fetch('https://react-course-http-bf9b0-default-rtdb.firebaseio.com/');
+      if (!responce.ok) { throw new Error('Something go wrong...') }
+      const data = await responce.json();
+      setJokes(data);      
+    } catch(err) {
+      setError(err.message);
+    }
     setIsLoading(false);
-  } 
+  },[]); 
 
+  useEffect(() => {
+    fetchJokesHandler();
+  },[fetchJokesHandler])
+  
+  const addJokeHandler = (joke) => {
+    console.log(joke)
+  }
+
+  let content = <p> Jokes not found... </p>;
+  
+  if(jokes.length > 0) {
+    content = <JokeList jokes={jokes} />;
+  }
+
+  if(error) {
+    content = <p>{error}</p>;
+  }
+
+  if(isLoading) {
+    content = <p> Jokes loading... </p>;
+  }
     
   return (
     <React.Fragment>
       <section>
+        <AddJoke onAddJoke={addJokeHandler} />
+      </section>
+      <section>
         <button onClick={fetchJokesHandler}>Fetch Jokes</button>
       </section>
       <section>
-        {!isLoading && jokes.length > 0 && <JokeList jokes={jokes} />}
-        {!isLoading && jokes.length === 0 && <p> Jokes not found... </p>}
+        {content}
+        {/* {!isLoading && jokes.length > 0 && <JokeList jokes={jokes} />}
+        {!isLoading && jokes.length === 0 && !error && <p> Jokes not found... </p>}
         {isLoading && <p> Jokes loading... </p>}
+        {!isLoading && error && <p>{error}</p>} */}
       </section>
     </React.Fragment>
   );
