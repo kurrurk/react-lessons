91347bc9c8718a0148003a873fecc57aa5e7756a
VasilyShat <vasily.shatalkin.job@gmail.com>
2024-12-08 00:04:27 +0100

    custom-hooks-one-more-example and working-with-user-input-starter (Lessons 170-178 : Custom Hooks и работа с формами)

--- custom-hooks-one-more-example/src/App.js
+++ custom-hooks-one-more-example/src/App.js
@@ -1,43 +1,58 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState} from "react";
 
 import Products from "./components/Products/Products";
 import NewProduct from "./components/NewProduct/NewProduct";
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [products, setProducts] = useState([]);
 
-  const fetchProducts = async (productText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-course-http-bf9b0-default-rtdb.firebaseio.com/products.json"
-      );
+  const [products, setProducts] = useState([]);  
 
-      if (!response.ok) {
-        throw new Error("Ошибка запроса.");
-      }
+  const {isLoading, error, sendHttpRequest: fetchProducts} = useHttp()
 
-      const data = await response.json();
+  // const fetchProducts = async (productText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       "https://react-course-http-bf9b0-default-rtdb.firebaseio.com/products.json"
+  //     );
 
-      const loadedProducts = [];
+  //     if (!response.ok) {
+  //       throw new Error("Ошибка запроса.");
+  //     }
 
-      for (const productKey in data) {
-        loadedProducts.push({ id: productKey, text: data[productKey].text });
-      }
+  //     const data = await response.json();
 
-      setProducts(loadedProducts);
-    } catch (err) {
-      setError(err.message || "Что-то пошло не так...");
-    }
-    setIsLoading(false);
-  };
+  //     const loadedProducts = [];
+
+  //     for (const productKey in data) {
+  //       loadedProducts.push({ id: productKey, text: data[productKey].text });
+  //     }
+
+  //     setProducts(loadedProducts);
+  //   } catch (err) {
+  //     setError(err.message || "Что-то пошло не так...");
+  //   }
+  //   setIsLoading(false);
+  // };
 
   useEffect(() => {
-    fetchProducts();
-  }, []);
+    const manageProducts = (productsData) => {
+
+      const loadedProducts = [];
+      
+      for (const productKey in productsData) {
+          loadedProducts.push({ id: productKey, text: productsData[productKey].text });
+      }
+  
+      setProducts(loadedProducts);
+  
+    }
+    fetchProducts({
+      url: "https://react-course-http-bf9b0-default-rtdb.firebaseio.com/products.json"
+    },manageProducts);
+  }, [fetchProducts]);
 
   const productAddHandler = (product) => {
     setProducts((prevProducts) => prevProducts.concat(product));
--- custom-hooks-one-more-example/src/components/NewProduct/NewProduct.js
+++ custom-hooks-one-more-example/src/components/NewProduct/NewProduct.js
@@ -1,41 +1,53 @@
-import { useState } from "react";
-
 import Section from "../UI/Section";
 import ProductForm from "./ProductForm";
+import useHttp from "../../hooks/use-http";
 
 const NewProduct = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const {isLoading, error, sendHttpRequest: sendProduct} = useHttp();
+
+  const createProduct = (productText,productData) => {
+    const generatedId = productData.name;
+    const createdProduct = { id: generatedId, text: productText };
+
+    props.onAddProduct(createdProduct);
+  }
 
   const enterProductHandler = async (productText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-course-http-bf9b0-default-rtdb.firebaseio.com/products.json",
-        {
-          method: "POST",
-          body: JSON.stringify({ text: productText }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+    sendProduct({
+      url: "https://react-course-http-bf9b0-default-rtdb.firebaseio.com/products.json",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: { text: productText },          
+    }, createProduct.bind(null, productText));
 
-      if (!response.ok) {
-        throw new Error("Ошибка запроса.");
-      }
+    // try {
+    //   const response = await fetch(
+    //     "https://react-course-http-bf9b0-default-rtdb.firebaseio.com/products.json",
+    //     {
+    //       method: "POST",
+    //       body: JSON.stringify({ text: productText }),
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //     }
+    //   );
 
-      const data = await response.json();
+    //   if (!response.ok) {
+    //     throw new Error("Ошибка запроса.");
+    //   }
 
-      const generatedId = data.name;
-      const createdProduct = { id: generatedId, text: productText };
+    //   const data = await response.json();
 
-      props.onAddProduct(createdProduct);
-    } catch (e) {
-      setError(e.message || "Что-то пошло не так...");
-    }
-    setIsLoading(false);
+    //   // const generatedId = data.name;
+    //   // const createdProduct = { id: generatedId, text: productText };
+
+    //   // props.onAddProduct(createdProduct);
+    // } catch (e) {
+    //   setError(e.message || "Что-то пошло не так...");
+    // }
+
   };
 
   return (
