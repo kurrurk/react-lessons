29518f6bc4f650a65263f63349e3873c75b6e77a
VasilyShat <kuremkarmerurk@gmail.com>
2026-06-02 22:55:54 +0200

    Lesson 005: Added examples demonstrating how to pass data between React components using props.

---
+++ lesson-005-passing_data_via_props/src/App.js
@@ -0,0 +1,44 @@
+import CostItem from "./components/CostItem/CostItem";
+
+function App() {
+  const costs = [
+    {
+      date: new Date(2026, 5, 12),
+      description: "Холодильник",
+      amount: 999.99,
+    },
+    {
+      date: new Date(2026, 11, 25),
+      description: "MacBook",
+      amount: 1071.54,
+    },
+    {
+      date: new Date(2026, 3, 15),
+      description: "Backpack",
+      amount: 42.99,
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <CostItem
+        date={costs[0].date}
+        description={costs[0].description}
+        amount={costs[0].amount}
+      ></CostItem>
+      <CostItem
+        date={costs[1].date}
+        description={costs[1].description}
+        amount={costs[1].amount}
+      ></CostItem>
+      <CostItem
+        date={costs[2].date}
+        description={costs[2].description}
+        amount={costs[2].amount}
+      ></CostItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ lesson-005-passing_data_via_props/src/components/CostItem/CostItem.js
@@ -0,0 +1,15 @@
+import "./CostItem.css";
+function CostItem(props) {
+  return (
+    <div className="cost-item">
+      {/* всегда должен быть корневой элемент. */}
+      <div>{props.date.toISOString()}</div>
+      <div className="cost-item__description">
+        <h2>{props.description}</h2>
+        <div className="cost-item__price">€{props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default CostItem;
