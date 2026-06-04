0de57ffb1ca3678de8eb2af86040433a0ead2ac8
VasilyShat <kuremkarmerurk@gmail.com>
2026-06-04 12:26:57 +0200

    Lesson 009: Add examples of arrow function

---
+++ lesson-009-arrow_functions/src/components/Costs/CostItem/CostItem.js
@@ -0,0 +1,17 @@
+import CostDate from "../CostDate/CostDate";
+import Card from "../../UI/Card/Card";
+import "./CostItem.css";
+const CostItem = (props) => {
+  return (
+    <Card className="cost-item">
+      {/* всегда должен быть корневой элемент. */}
+      <CostDate date={props.date} />
+      <div className="cost-item__description">
+        <h2>{props.description}</h2>
+        <div className="cost-item__price">€{props.amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default CostItem;
