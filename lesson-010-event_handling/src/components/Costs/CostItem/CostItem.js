14c447fdd181bef0342abc11acdb6c2299ab975f
VasilyShat <kuremkarmerurk@gmail.com>
2026-06-06 14:36:58 +0200

    Lesson 010: Added examples demonstrating basic event handling in React using onClick event handlers.

---
+++ lesson-010-event_handling/src/components/Costs/CostItem/CostItem.js
@@ -0,0 +1,25 @@
+import CostDate from "../CostDate/CostDate";
+import Card from "../../UI/Card/Card";
+import "./CostItem.css";
+
+const CostItem = (props) => {
+  const changeDescription = (props) => {
+    console.log("Click!");
+  };
+
+  return (
+    <Card className="cost-item">
+      {/* всегда должен быть корневой элемент. */}
+      <CostDate date={props.date} />
+      <div className="cost-item__description">
+        <h2>{props.description}</h2>
+        <div className="cost-item__price">€{props.amount}</div>
+      </div>
+      <button className="btn btn-primary ml-2" onClick={changeDescription}>
+        Изменить Описание
+      </button>
+    </Card>
+  );
+};
+
+export default CostItem;
