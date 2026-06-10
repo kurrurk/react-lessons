e24ed811afdf9d8f192becfa63faab94b332cb6a
VasilyShat <kuremkarmerurk@gmail.com>
2026-06-10 20:48:12 +0200

    Lesson 018: Using Keys in Lists

--- lesson-018-using_lists_with_state/src/components/Costs/CostsList/CostsList.js
+++ lesson-018-using_lists_with_state/src/components/Costs/CostsList/CostsList.js
@@ -16,6 +16,7 @@ const CostsList = (props) => {
         <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
         {props.costs.map((cost) => (
           <CostItem
+            key={cost.id}
             date={cost.date}
             description={cost.description}
             amount={cost.amount}
