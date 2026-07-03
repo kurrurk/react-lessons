e5a04c8e7b91d4a006d20c3f9da0f3f03c0f0001
VasilyShat <kuremkarmerurk@gmail.com>
2026-07-04 01:16:54 +0200

    Lesson 026: CSS Modules

---
+++ lesson-026-Using_CSS_Modules/src/components/Tasks/TaskInput/TaskInput.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Button from "../../UI/Button/Button";
+import styles from "./TaskInput.module.css";
+
+const TaskInput = (props) => {
+  const [inputText, setInputText] = useState("");
+  const [isInputValid, setIsInputValid] = useState(true);
+
+  const taskInputChangeHandler = (event) => {
+    if (inputText.trim().length > 0) {
+      setIsInputValid(true);
+    }
+    setInputText(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (inputText.trim().length === 0) {
+      setIsInputValid(false);
+      return;
+    }
+    props.onAddTask(inputText);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div
+        className={`${styles["form-control"]} ${!isInputValid && styles.invalid}`}
+        invalid={!isInputValid}
+      >
+        <label>Задачи</label>
+        <input type="text" onChange={taskInputChangeHandler} />
+      </div>
+      <Button type="submit">Добавить Задачу</Button>
+    </form>
+  );
+};
+
+export default TaskInput;
